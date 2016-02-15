29a494fefc9b1e877a62ba68aee5cafc82102419
Christian Nagel <christian@christiannagel.com>
2016-02-15 13:39:54 +0100

    thread properties in a single line

--- ThreadSample/ThreadSample/Program.cs
+++ ThreadSample/ThreadSample/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ThreadSample
 {
@@ -16,6 +17,8 @@ namespace ThreadSample
             t1.SetApartmentState(ApartmentState.STA);
             t1.IsBackground = false;
             t1.Start();
+
+            new Thread(() => WriteLine("in a thread")){ IsBackground = true }.Start();
             Console.WriteLine("end of main");
         }
 
