f6352ab2aa076c973730e16479b2104f78decadf
Christian Nagel <christian@christiannagel.com>
2016-02-16 14:16:34 +0100

    WinFormsSample

---
+++ AVeryOldWinFormsApp/AVeryOldWinFormsApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AVeryOldWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            Task<int> t1 = Task<int>.Run<int>(() =>
+            {
+                return Add(x, y);
+            });
+
+            t1.ContinueWith(t =>
+            {
+                var result = t.Result;
+                // label1.Text = result.ToString();
+                this.Invoke(new Action<string>(SetLabel), result.ToString());
+            });
+        }
+
+        private void SetLabel(string text)
+        {
+            this.label1.Text = text;
+        }
+
+        public int Add(int x, int y)
+        {
+            Thread.Sleep(4000);
+            return x + y;
+        }
+    }
+}
