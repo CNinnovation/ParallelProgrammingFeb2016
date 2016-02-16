d9497f093a2ec7d902e7b4fb4d675ec0b164ac6c
Christian Nagel <christian@christiannagel.com>
2016-02-16 11:20:21 +0100

    aggregation sample

---
+++ AggregationSample/AggregationSample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AggregationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParallelForDemo();
+        }
+
+        private static void ParallelForDemo()
+        {
+            
+            List<int> results = new List<int>();
+            object syncResults = new object();
+
+
+
+            int sum = 0;
+            Parallel.For(0, 100,
+                () =>
+                {
+                    ShowInfo("local init");
+                    return 0;
+                },
+                (x, pls, a) =>
+                {
+                    ShowInfo($"body a: {a}, x: {x}");
+                    return a +x;
+                },
+                a =>
+                {
+                    ShowInfo($"end a: {a}");
+                    lock(syncResults)
+                    {
+                        results.Add(a);
+                    }
+
+                    Interlocked.Add(ref sum, a);
+                }
+                );
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void ShowInfo(string info)
+        {
+            Console.WriteLine($"{info} {Task.CurrentId} {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
