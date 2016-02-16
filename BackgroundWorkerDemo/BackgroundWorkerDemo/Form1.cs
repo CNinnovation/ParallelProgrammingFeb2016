e6e761f4c8cf581eb91a9acd85caff227ae41c15
Christian Nagel <christian@christiannagel.com>
2016-02-16 16:00:12 +0100

    background worker

---
+++ BackgroundWorkerDemo/BackgroundWorkerDemo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+
+            backgroundWorker1.RunWorkerAsync(Tuple.Create(x, y));
+        }
+
+        private void OnCalculation(object sender, DoWorkEventArgs e)
+        {
+            // background thread
+            Tuple<int, int> input = (Tuple<int, int>)e.Argument;
+            Thread.Sleep(1000);
+            int result = input.Item1 + input.Item2;
+            e.Result = result;
+        }
+
+        private void OnCalculationCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            int result  = (int)e.Result;
+            label1.Text = result.ToString();
+
+        }
+    }
+}
