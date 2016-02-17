cca22eb2cd0479e7febb1ccd9a23d02922ec6b74
Christian Nagel <christian@christiannagel.com>
2016-02-17 15:20:20 +0100

    WPF sample

---
+++ MyFirstWPFApp/MyFirstWPFApp/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWPFApp
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Title = "Professional C#";
+            TheBook = new Book { Title = "Universal Apps", Publisher = "Self" };
+        }
+        public string Title { get; set; }
+
+        public Book TheBook { get; set; }
+    }
+}
